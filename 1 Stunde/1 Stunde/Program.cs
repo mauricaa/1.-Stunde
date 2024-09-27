aa1ad03464a1d1d9ba029cdeea2ce9d43e03ba38
maurice <maurice@htl.do>
2024-09-27 07:07:44 +0200

    aufgabe 1 finished

--- 1 Stunde/1 Stunde/Program.cs
+++ 1 Stunde/1 Stunde/Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("Döner oder so Onur");
+bool eingabe = true;
 
+Console.WriteLine("schreib etwas");
+
+while (eingabe)
+{
+    string input = Console.ReadLine();
+    string stop = "beenden";
+    string lowerInput = input.ToLower();
+    Console.WriteLine(input);
+    if (lowerInput == stop)
+    {
+        eingabe = false;
+    }
+}
