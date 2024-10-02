7214fa07a74e3e7416045a8b69bc0abaf3b21c33
maurice <maurice@htl.do>
2024-10-02 22:46:48 +0200

    Aufgabe 2 finished

--- 1 Stunde/1 Stunde/Program.cs
+++ 1 Stunde/1 Stunde/Program.cs
@@ -1,18 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
 bool eingabe = true;
 
-Console.WriteLine("schreib etwas");
-
+Console.WriteLine("Gib eine natürliche Zahl ein oder schreibe 'beenden', um das Programm zu beenden:");
 while (eingabe)
 {
     string input = Console.ReadLine();
     string stop = "beenden";
     string lowerInput = input.ToLower();
-    Console.WriteLine(input);
+
     if (lowerInput == stop)
     {
         eingabe = false;
     }
+    else
+    {
+        try
+        {
+            int zahl = int.Parse(input);
+            if (zahl > 0)
+            {
+                Console.WriteLine($"Die Zahl {zahl} wurde eingegeben");
+            }
+            else
+            {
+                Console.WriteLine("gibe eine Zahl ein natürlche Zahl ein die grösser als 0 ist");
+            }
+        }
+             catch (FormatException)
+        {
+            Console.WriteLine("Ungültige Eingabe. Gib eine neue gültige Zahl ein.");
+        }
+    }
 }
+Console.WriteLine("Programm beendet.");
