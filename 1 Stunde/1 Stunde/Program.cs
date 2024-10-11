24de79894446e478c3f5064c6733595d0db69365
maurice <maurice@htl.do>
2024-10-11 14:21:50 +0200

    aufgabe 5 noch in vollem gange

--- 1 Stunde/1 Stunde/Program.cs
+++ 1 Stunde/1 Stunde/Program.cs
@@ -1,68 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-bool eingabe = true;
+﻿// Siehe https://aka.ms/new-console-template für weitere Informationen
+Console.WriteLine("Schreibe einen Satz der a, e, i, o, u enthält");
+string inputText = Console.ReadLine();
+string lowerInput = inputText.ToLower(); 
 
-Console.WriteLine("Gib eine Zahl ein oder schreibe 'beenden', um das Programm zu beenden:");
-while (eingabe)
+foreach (char letter in lowerInput)
 {
-    string input = Console.ReadLine();
-    string stop = "beenden";
-    string lowerInput = input.ToLower();
-
-    if (lowerInput == "beenden")
+    if (lowerInput.ContainsKey(letter))
     {
-        eingabe = false;
-    }
-    else if (int.TryParse(input, out int intWert))
-    {
-        if (intWert > 0)
-        {
-            Console.WriteLine($"Die eingegebene Zahl ist ein Integer: {intWert}");
-
-            // Auswahl der mathematischen Operation
-            Console.WriteLine("Geben Sie eine Zahl zwischen 1 und 3 ein, um eine Mathematische Operation auszuwählen:");
-            Console.WriteLine("1) Quadrat");
-            Console.WriteLine("2) Wurzel");
-            Console.WriteLine("3) Fakultät");
-            string operation = Console.ReadLine();
-
-            switch (operation)
-            {
-                case "1":
-                    Console.WriteLine($"Das Quadrat von {intWert} ist: {intWert * intWert}");
-                    break;
-
-                case "2":
-                    Console.WriteLine($"Die Wurzel von {intWert} ist: {Math.Sqrt(intWert)}");
-                    break;
-
-                case "3":
-                    Console.WriteLine($"Die Fakultät von {intWert} ist: {Fakultaet(intWert)}");
-                    break;
-
-                default:
-                    Console.WriteLine("Ungültige Auswahl. Bitte eine Zahl zwischen 1 und 3 wählen.");
-                    break;
-            }
-        }
-        else
-        {
-            Console.WriteLine("Gib eine natürliche Zahl ein, die größer als 0 ist.");
-        }
-    }
-    else
-    {
-        Console.WriteLine("Ungültige Eingabe. Gib eine neue gültige Zahl ein.");
+        lowerInput[letter]++;
     }
 }
 
-Console.WriteLine("Programm beendet.");
-
-static long Fakultaet(int n)
+foreach (var vowel in lowerInput)
 {
-    long result = 1;
-    for (int i = 1; i <= n; i++)
-    {
-        result *= i;
-    }
-    return result;
+    Console.WriteLine($"Anzahl {vowel.Key}: {vowel.Value}");
 }
+    }
+}
+
